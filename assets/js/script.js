1804bda133b6a197fa2df2004efcbc6fac42470f
Tanner Griffin <tannermgriffin@gmail.com>
2020-05-19 17:43:03 -0500

    Created onclick function for City input. Adds input text to new list item, saves to localStorage. Max number of list items set to 8

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// on search button click, set new var to input value
+// print to new list item and save to localStorage
+
+$('#button-city').on('click', function() {
+    
+    var newCity = $('input').val();
+    var newListItem = $(`<li class="list-group-item">${newCity}</li>`);
+    if ($('.list-group-item').length === 8) {
+        $('.list-group-item').last().remove();
+    }
+    $('.city-list').prepend(newListItem);
+    localStorage.setItem('recentCity', JSON.stringify(newCity));
+})
