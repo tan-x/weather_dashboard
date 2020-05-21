4e92036a7a858d6181368c2c6af690d3e918108f
Tanner Griffin <tannermgriffin@gmail.com>
2020-05-20 23:54:07 -0500

    added forecast function, added spans to forecast elements for printing forecast data

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,12 +20,28 @@ function getData(newCity) {
     }).then(function(response) {
         var m = moment().format('MM/DD/YYYY');
         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
         console.log(response);
         $('#city').text(response.name + ' ' + m);
         $('.temp').html(tempF.toFixed(0) + '&deg;');
         $('.humid').html(response.main.humidity + '%');
         $('.wind').html((response.wind.speed * 2.237).toFixed(0) + ' MPH');
         $('.uv').html();
+        getForecast(lat, lon);
+    })
+}
+
+function getForecast(lat, lon) {
+    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=18bbe1392a0905c921468fb3545d2bfb`;
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response) {
+        for (i = 0; i < 5; i++) {
+            var tempF = (response.list[i].main.temp_max - 273.15) * 1.80 + 32;
+            $(`span[data-foreTemp="${i}"`).text(tempF.toFixed(0));
+        }
     })
 }
 
